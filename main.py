37b5f9353c3cb856270f4cbf69f19b0bbaea5ead
Билигма <z.bilyashik@gmail.com>
2024-04-23 19:43:29 +0900

    в лс отправляет роль

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import random
 
 import telebot
 import telegram
 from telebot import types  # для указание типов
+from telegram.ext import ConversationHandler
 
 bot = telebot.TeleBot("6443463170:AAH1a3G2p72uA2INs3_5_WHgDHB7HlnHIwM")
 
@@ -28,18 +30,28 @@ def func(message):
         bot.send_message(message.chat.id, text="На сколько человек?", reply_markup=markup)
 
     elif message.text == '3-6':
-        bot.send_message(message.chat.id, "В таком случае, роль мафии отводится одному игроку.")
+        bot.send_message(message.chat.id, "В таком случае роль мафии отводится одному игроку. Всем игрокам необходимо "
+                                          "поставить +")
+    elif message.text == '+':
+        roles = ['mafia', 'peace']
+        bot.send_message(message.from_user.id, f"Привет! Твоя роль: {random.choices(roles)}")
+
     elif message.text == "7-9":
-        bot.send_message(message.chat.id, "В таком случае, роль мафии отводится двум игрокам.")
+        bot.send_message(message.chat.id, "В таком случае роль мафии отводится двум игрокам.")
     elif message.text == "10-11":
-        bot.send_message(message.chat.id, "В таком случае, роль мафии отводится трем игрокам.")
+        bot.send_message(message.chat.id, "В таком случае роль мафии отводится трем игрокам.")
     elif message.text == "больше 12":
         bot.send_message(message.chat.id, "Напишите точное количество.")
     elif message.text:
-        bot.send_message(message.chat.id, f'В таком случае, роль мафии отводится {round(int(message.text)/4)} игрокам.')
-
+        bot.send_message(message.chat.id,
+                         f'В таком случае, роль мафии отводится {round(int(message.text) / 4)} игрокам.')
     else:
         bot.send_message(message.chat.id, text="На такую команду я не запрограммирован")
 
 
 bot.polling(none_stop=True)
+
+
+async def stop(update):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
